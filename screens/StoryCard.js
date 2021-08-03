cbbbafe350697c7d503676666a5e3f1f29874751
kolamax25 <75153987+kolamax25@users.noreply.github.com>
2021-08-02 19:23:21 -0700

    Add files via upload

---
+++ screens/StoryCard.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.cardContainer}>
+            <Image
+              source={require("../assets/story_image_1.png")}
+              style={styles.storyImage}
+            ></Image>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.storyTitleText}>
+                {this.props.story.title}
+              </Text>
+              <Text style={styles.storyAuthorText}>
+                {this.props.story.author}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {this.props.story.description}
+              </Text>
+            </View>
+            <View style={styles.actionContainer}>
+              <View style={styles.likeButton}>
+                <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                <Text style={styles.likeText}>12k</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
